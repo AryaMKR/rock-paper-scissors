e4f8697d79188c942aa6d60e17f7a29f510a1f1d
arya.rogne <215011823+AryaMKR@users.noreply.github.com>
2025-08-04 13:25:55 -0500

    Adds getComputerChoice func with random selection

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+/* console.log("Hello World"); */
+
+function getComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * 3); // 0, 1, or 2
+  if (randomNumber === 0) {
+    return "rock";
+  } else if (randomNumber === 1) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+console.log(getComputerChoice())
