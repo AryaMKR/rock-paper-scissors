c71b814fa545573ad6068949d27f10e77f61554f
arya.rogne <215011823+AryaMKR@users.noreply.github.com>
2025-08-04 14:14:48 -0500

    Adds game logic w. user input and result function

--- script.js
+++ script.js
@@ -11,3 +11,31 @@ function getComputerChoice() {
   }
 }
 console.log(getComputerChoice())
+
+function getHumanChoice() {
+    const choice = prompt("Choose rock, paper, or scissors:");
+  return choice;
+}
+
+let HumanChoice = getHumanChoice();
+
+console.log("Playerpicked:", HumanChoice)
+
+function result() {
+  const computerChoice = getComputerChoice();
+
+    if (HumanChoice === getComputerChoice()) {
+    return "It is a tie"
+}
+else if (
+    (HumanChoice === "rock" && computerChoice === "scissors") ||
+    (HumanChoice === "paper" && computerChoice === "rock") ||
+    (HumanChoice === "scissors" && computerChoice === "paper")
+) {
+    return "You win!";
+} else {
+    return "You lose!";
+}
+}
+
+console.log(result())
